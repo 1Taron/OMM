e2d56a9985d012a30ce8c4a5b378daced545dd27
Ukdam <90294651+Ukdam@users.noreply.github.com>
2023-10-11 17:18:38 +0900

    로그인 페이지 아이콘 추가
    로그인 페이지 아이콘 추가

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,37 +1,46 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import "../css/Header.css";
-import "../css/login.css"
+import "../css/login.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 export default function Login() {
-    const { username, setUserName } = useState("");
-    const { password, setpassword } = useState("");
+  const { username, setUserName } = useState("");
+  const { password, setpassword } = useState("");
 
-    return (
-        <main className="layout_Mw">
-            <Header />
-            <Outlet />
-            <form className="login">
-                <div className="login_container">
-                    <img className="mainImage_1" alt="MainLogo" src="img/logo.svg" />
-                    <input
-                        type={"text"}
-                        placeholder="아이디"
-                        value={username}
-                    ></input>
-                    <input
-                        type={"text"}
-                        placeholder="비밀번호"
-                        value={password}
-                    ></input>
-                    <button className="login_btn" >로그인</button>
-                    <Link to="/register">
-                        <button className="register_btn">회원가입</button>
-                    </Link>
-                </div>
-            </form>
-        </main>
-    );
+  return (
+    <main className="layout_Mw">
+      <Header />
+      <Outlet />
+      <form className="login">
+        <div className="login_container">
+          <img className="mainImage_1" alt="MainLogo" src="img/logo.svg" />
+          <div className="login_box">
+            <div className="login_idInput">
+              <i class="fa-solid fa-user"></i>
+              <input
+                type={"text"}
+                placeholder="아이디"
+                value={username}
+              ></input>
+            </div>
+            <div className="login_pwInput">
+              <i class="fa-solid fa-lock"></i>
+              <input
+                type={"text"}
+                placeholder="비밀번호"
+                value={password}
+              ></input>
+            </div>
+          </div>
+          <button className="login_btn">로그인</button>
+          <Link to="/register">
+            <button className="register_btn">회원가입</button>
+          </Link>
+        </div>
+      </form>
+    </main>
+  );
 }
