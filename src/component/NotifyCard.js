67d888616ee5ebd51b8171b6be63af1b81354061
Ukdam <grande125@naver.com>
2023-10-12 10:43:21 +0900

    결제페이지 수정
    결제페이지 수정

--- src/component/NotifyCard.js
+++ src/component/NotifyCard.js
@@ -7,7 +7,7 @@ export default function NotifyCard() {
           <span className="notify_time font_01">2023-10-11 / 12:20</span>
         </div>
         <div className="card_contentbox">
-          <p className="font_01 notify_content">
+          <p className="font_02 notify_content">
             주문하신 음식이 접수 완료되었습니다.
           </p>
         </div>
--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -1,7 +1,10 @@
+import React from "react";
 import Header from "../component/Header";
 import "../css/Payment.css";
+import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
 
 export default function PaymentPage() {
+  const [value, setValue] = React.useState("1");
   return (
     <>
       <Header />
@@ -21,6 +24,26 @@ export default function PaymentPage() {
               <p>재료3</p>
             </div>
           </div>
+        </div>
+
+        <div className="pay_remain_container">
+          <div className="pay_delivery_box">
+            <p className="pay_subtitle">배송지</p>
+            <RadioGroup onChange={setValue} value={value} m="10px">
+              <Stack direction="row">
+                <Radio value="1">기본 배송지</Radio>
+                <Radio value="2">신규 입력</Radio>
+              </Stack>
+            </RadioGroup>
+            <div className="pay_delivery_adress">
+              <p className="font_02">전라북도 익산시 익산대로 460</p>
+            </div>
+            <div className="pay_delivery_request">
+              <p>배달 시 요청 사항</p>
+              <input type="text" className="font_02" />
+            </div>
+          </div>
+          <div className="pay_privacy_box">개인정보</div>
         </div>
       </div>
     </>
