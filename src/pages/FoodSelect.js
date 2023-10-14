3587d7d25e651f09099fc7d1c58363ee3e5c0617
1Taron <she11466@naver.com>
2023-10-14 18:03:46 +0900

    이건좀 오반데.... 1차완료

--- src/pages/FoodSelect.js
+++ src/pages/FoodSelect.js
@@ -2,22 +2,27 @@ import Header from "../component/Header";
 import "../css/FoodSelect.css";
 import { Outlet } from "react-router-dom";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useColorModeValue } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Grid, Pagination } from 'swiper/modules';
+import { Grid } from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/grid';
-import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 export default function FoodSelect() {
     const colors = useColorModeValue(
-        ['red.50', 'teal.50', 'blue.50'],
+        ['#ffEFC8', 'teal.50', 'blue.50'],
         ['red.900', 'teal.900', 'blue.900'],
     )
 
     const [tabIndex, setTabIndex] = useState(0)
     const bg = colors[tabIndex]
+
+
+
+
     return (
         <>
             <Header />
@@ -38,30 +43,76 @@ export default function FoodSelect() {
                                 slidesPerView={4}
                                 grid={{
                                     rows: 2,
+                                    fill: "row",
                                 }}
-                                spaceBetween={10}
-                                pagination={{
-                                    clickable: true,
-                                }}
-                                modules={[Grid, Pagination]}
+                                spaceBetween={30}
+
+                                modules={[Grid]}
                                 className="mySwiper"
                             >
-                                <SwiperSlide>Slide 1</SwiperSlide>
-                                <SwiperSlide>Slide 2</SwiperSlide>
-                                <SwiperSlide>Slide 3</SwiperSlide>
-                                <SwiperSlide>Slide 4</SwiperSlide>
-                                <SwiperSlide>Slide 5</SwiperSlide>
-                                <SwiperSlide>Slide 6</SwiperSlide>
-                                <SwiperSlide>Slide 7</SwiperSlide>
-                                <SwiperSlide>Slide 8</SwiperSlide>
-                                <SwiperSlide>Slide 9</SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <button>
+                                        <img className="ingrediant_img" src="img/sangchu.png"></img>
+                                        <div className="ingrediant_name">상추</div>
+                                    </button>
+                                </SwiperSlide>
                             </Swiper>
                         </TabPanel>
                         <TabPanel>Are 1, 2, 3</TabPanel>
                         <TabPanel>Red, yellow and blue.</TabPanel>
                     </TabPanels>
                 </Tabs>
-
+                <div className="btn_container">
+                    <button className="Select_btn">선택 완료</button>
+                </div>
             </main>
         </>
     );
