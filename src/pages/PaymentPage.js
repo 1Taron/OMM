8a49f3d570c7c9d5d3c3cdc28a6d1ee2945df4ca
Ukdam <90294651+Ukdam@users.noreply.github.com>
2023-10-17 21:31:20 +0900

    payment 배달/포장 분리
    payment 배달/포장 분리

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -49,191 +49,190 @@ export default function PaymentPage() {
       setCustomInput(inputText);
     }
   };
+
+  async function delivery_pay(e) {
+    e.preventDefault();
+
+    // const response = await fetch("http://localhost:4000/payment_delivery", {
+    //   method: "POST",
+    //   body: JSON.stringify({ username, password }),
+    //   headers: { "Content-Type": "application/json" },
+    //   credentials: "include",
+    // });
+
+    // if (response.ok) {
+    //   response.json().then((userInfo) => {
+    //     setUserInfo(userInfo);
+    //     setRedirect(true);
+    //   });
+    // } else {
+    //   alert("로그인 실패");
+    // }
+  }
+
   return (
     <>
       <Header />
-
       <div className="payMain_container font_01">
         <div className="pay_branch_container font_03">
           <p>익산점</p>
         </div>
 
-        <div className="pay_choice_container">
-          <Tabs isFitted variant="enclosed" bg={"white"}>
-            <TabList mb="1em">
-              <Tab fontSize={"1.4rem"}>
-                배달 <p className="pay_tabsubtitle">(예상시간 : 40 ~ 50분)</p>
-              </Tab>
-              <Tab fontSize={"1.4rem"}>
-                <Link to={"/payment_pickup"}>
-                  포장 <p className="pay_tabsubtitle"> (픽업시간 : 10분)</p>
-                </Link>
-              </Tab>
-            </TabList>
-            <TabPanels>
-              {/* 배달 */}
-              <TabPanel>
-                {/* 주소 */}
-                <div className="pay_adress_container">
-                  <div className="pay_adress_left">--</div>
-                  <div className="pay_adress_center">
-                    <p>전라북도 익산시 익산대로 460(으)로 배달</p>
-                    <p className="pay_tabsubtitle font_02">
-                      ------ 자세한 주소 ------
-                    </p>
-                  </div>
-                  <div className="pay_adress_right">
-                    <Button
-                      onClick={onOpen}
-                      colorScheme="black"
-                      variant="outline"
-                      fontSize={"1rem"}
-                      color={"gray"}
-                      height={"25px"}
-                      width={"70px"}
-                    >
-                      <p className="font_02">변경</p>
-                    </Button>
-                  </div>
-                </div>
+        <div className="__payMain_container">
+          <div className="pay_choice_container">
+            <button className="__pay_delivery_btn01">
+              배달
+              <p className="pay_tabsubtitle">(예상시간 : 40 ~ 50분)</p>
+            </button>
+            <Link to={"/payment_pickup"}>
+              <button className="__pay_delivery_btn02">
+                포장
+                <p className="pay_tabsubtitle">(픽업시간 : 10분)</p>
+              </button>
+            </Link>
+          </div>
+          {/* 주소 */}
+          <div className="pay_adress_container">
+            <div className="pay_adress_left">--</div>
+            <div className="pay_adress_center">
+              <p>전라북도 익산시 익산대로 460(으)로 배달</p>
+              <p className="pay_tabsubtitle font_02">
+                ------ 자세한 주소 ------
+              </p>
+            </div>
+            <div className="pay_adress_right">
+              <Button
+                onClick={onOpen}
+                colorScheme="black"
+                variant="outline"
+                fontSize={"1rem"}
+                color={"gray"}
+                height={"25px"}
+                width={"70px"}
+              >
+                <p className="font_02">변경</p>
+              </Button>
+            </div>
+          </div>
 
-                {/* 장바구니 */}
-                <div className="pay_menu_container">
-                  <div className="pay_menu_box">
-                    <MenuCard />
-                    <MenuCard />
-                  </div>
-                  <div className="pay_menu_add">
-                    <Link to={"/"}>메뉴 추가하기</Link>
-                  </div>
-                </div>
+          {/* 장바구니 */}
+          <div className="pay_menu_container">
+            <div className="pay_menu_box">
+              <MenuCard />
+              <MenuCard />
+            </div>
+            <div className="pay_menu_add">
+              <Link to={"/"}>메뉴 추가하기</Link>
+            </div>
+          </div>
 
-                {/* 요청사항 */}
-                <div className="pay_request_container">
-                  <p className="pay_req_title">요청사항</p>
-                  <div className="pay_req_default01 font_02">
-                    <Checkbox
-                      border={"gray"}
-                      colorScheme="blue"
-                      size="lg"
-                      spacing="1rem"
-                    >
-                      문 앞에 놓고, 문자주세요.
-                    </Checkbox>
-                  </div>
-                  <div className="pay_req_default02 font_02">
-                    <Checkbox
-                      border={"gray"}
-                      colorScheme="blue"
-                      size="lg"
-                      spacing="1rem"
-                    >
-                      일회용 수저, 포크가 필요해요.
-                    </Checkbox>
-                  </div>
-                  <div className="pay_req_choice">
-                    <Select
-                      placeholder="요청사항을 선택해주세요."
-                      bg={"initial"}
-                      fontSize={"1.2rem"}
-                      color={"gray"}
-                      size={"lg"}
-                      mt={"15px"}
-                      border={"none"}
-                    >
-                      <option value="option1">
-                        서두르지 않고 안전하게 와주세요.
-                      </option>
-                      <option value="option2">그냥 문 앞에 놓아 주세요.</option>
-                      <option value="option3">
-                        벨 누르기 전에 전화 주세요.
-                      </option>
-                      <option value="option4">
-                        도착 후 전화주시면 직접 받으러 갈게요.
-                      </option>
-                    </Select>
-                    <div className="request_directbox">
-                      <p>직접 입력</p>
-                      <textarea
-                        className="request_directinput"
-                        value={customInput}
-                        onChange={handleCustomInputChange}
-                        placeholder="요청사항을 입력해주세요."
-                      />
-                      <p>
-                        {customInput.length}/{maxLength}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                {/* 결제 수단 */}
-                <div className="pay_paymethod_container">
-                  <p className="pay_method_title">결제수단</p>
-                  <div className="pay_methid_radio">
-                    <RadioGroup onChange={setPaymentValue} value={paymentValue}>
-                      <Stack direction="row">
-                        <Radio
-                          value="1"
-                          size={"lg"}
-                          mr={"12rem"}
-                          spacing={"3"}
-                          border={"1px solid #d8d9da"}
-                        >
-                          <p className="pay_radio01 font_02">신용 카드</p>
-                        </Radio>
-                        <Radio
-                          value="2"
-                          size="lg"
-                          spacing={"3"}
-                          border={"1px solid #d8d9da"}
-                        >
-                          <p className="pay_radio01 font_02">현장 결재</p>
-                        </Radio>
-                      </Stack>
-                    </RadioGroup>
-                  </div>
-                </div>
-
-                {/* 금액 */}
-                <div className="pay_amount_container">
-                  <div className="pay_amount_detailbox">
-                    <p className="pay_detailkind">상품금액</p>
-                    <p className="pay_detailcash">{goodPrice} 원</p>
-                    <p className="pay_detailkind">추가금액</p>
-                    <p className="pay_detailcash">{extraPrice} 원</p>
-                    <p className="pay_detailkind">배달요금</p>
-                    <p className="pay_detailcash">{deliveryPrice} 원</p>
-                  </div>
-                  <div className="pay_amount_totalbox">
-                    <p className="pay_total">총 결제금액</p>
-                    <p className="pay_totalcash">
-                      {goodPrice + extraPrice + deliveryPrice} 원
-                    </p>
-                  </div>
-                </div>
-
-                {/* 버튼 */}
-                <Box
-                  as="button"
-                  width={"300px"}
-                  fontSize={"1.2rem"}
-                  p={3}
-                  color="white"
-                  fontWeight="reguiar"
-                  borderRadius="lg"
-                  bgGradient="linear(to-t, blue.500, blue.400, blue.500)"
-                  _hover={{
-                    bgGradient: "linear(to-t,  blue.400, blue.300, blue.400)",
-                  }}
+          {/* 요청사항 */}
+          <form onSubmit={delivery_pay}>
+            <div className="pay_request_container">
+              <p className="pay_req_title">요청사항</p>
+              <div className="pay_req_default01 font_02">
+                <Checkbox
+                  border={"gray"}
+                  colorScheme="blue"
+                  size="lg"
+                  spacing="1rem"
                 >
-                  결제하기
-                </Box>
-              </TabPanel>
-              {/* 포장 */}
-              <TabPanel></TabPanel>
-            </TabPanels>
-          </Tabs>
+                  문 앞에 놓고, 문자주세요.
+                </Checkbox>
+              </div>
+              <div className="pay_req_default02 font_02">
+                <Checkbox
+                  border={"gray"}
+                  colorScheme="blue"
+                  size="lg"
+                  spacing="1rem"
+                >
+                  일회용 수저, 포크가 필요해요.
+                </Checkbox>
+              </div>
+              <div className="pay_req_choice">
+                <Select
+                  placeholder="요청사항을 선택해주세요."
+                  bg={"initial"}
+                  fontSize={"1.2rem"}
+                  color={"gray"}
+                  size={"lg"}
+                  mt={"15px"}
+                  border={"none"}
+                >
+                  <option value="option1">
+                    서두르지 않고 안전하게 와주세요.
+                  </option>
+                  <option value="option2">그냥 문 앞에 놓아 주세요.</option>
+                  <option value="option3">벨 누르기 전에 전화 주세요.</option>
+                  <option value="option4">
+                    도착 후 전화주시면 직접 받으러 갈게요.
+                  </option>
+                </Select>
+                <div className="request_directbox">
+                  <p>직접 입력</p>
+                  <textarea
+                    className="request_directinput"
+                    value={customInput}
+                    onChange={handleCustomInputChange}
+                    placeholder="요청사항을 입력해주세요."
+                  />
+                  <p>
+                    {customInput.length}/{maxLength}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* 결제 수단 */}
+            <div className="pay_paymethod_container">
+              <p className="pay_method_title">결제수단</p>
+              <div className="pay_methid_radio">
+                <RadioGroup onChange={setPaymentValue} value={paymentValue}>
+                  <Stack direction="row">
+                    <Radio
+                      value="1"
+                      size={"lg"}
+                      mr={"12rem"}
+                      spacing={"3"}
+                      border={"1px solid #d8d9da"}
+                    >
+                      <p className="pay_radio01 font_02">신용 카드</p>
+                    </Radio>
+                    <Radio
+                      value="2"
+                      size="lg"
+                      spacing={"3"}
+                      border={"1px solid #d8d9da"}
+                    >
+                      <p className="pay_radio01 font_02">현장 결재</p>
+                    </Radio>
+                  </Stack>
+                </RadioGroup>
+              </div>
+            </div>
+
+            {/* 금액 */}
+            <div className="pay_amount_container">
+              <div className="pay_amount_detailbox">
+                <p className="pay_detailkind">상품금액</p>
+                <p className="pay_detailcash">{goodPrice} 원</p>
+                <p className="pay_detailkind">추가금액</p>
+                <p className="pay_detailcash">{extraPrice} 원</p>
+                <p className="pay_detailkind">배달요금</p>
+                <p className="pay_detailcash">{deliveryPrice} 원</p>
+              </div>
+              <div className="pay_amount_totalbox">
+                <p className="pay_total">총 결제금액</p>
+                <p className="pay_totalcash">
+                  {goodPrice + extraPrice + deliveryPrice} 원
+                </p>
+              </div>
+            </div>
+
+            {/* 버튼 */}
+            <button className="_payment_paybtn">결제하기</button>
+          </form>
         </div>
       </div>
 
--- src/pages/PaymentPage_Pickup.js
+++ src/pages/PaymentPage_Pickup.js
@@ -26,7 +26,7 @@ import {
   Radio,
   RadioGroup,
 } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 import { Search2Icon } from "@chakra-ui/icons";
 
@@ -49,6 +49,7 @@ export default function PaymentPage_Pickup() {
       setCustomInput(inputText);
     }
   };
+
   return (
     <>
       <Header />
@@ -57,138 +58,106 @@ export default function PaymentPage_Pickup() {
         <div className="pay_branch_container font_03">
           <p>익산점</p>
         </div>
+        <div className="__payMain_container">
+          <div className="pay_choice_container">
+            <Link to={"/payment_delivery"}>
+              <button className="__pay_pickup_btn01">
+                배달
+                <p className="pay_tabsubtitle">(예상시간 : 40 ~ 50분)</p>
+              </button>
+            </Link>
+            <button className="__pay_pickup_btn02">
+              포장
+              <p className="pay_tabsubtitle">(픽업시간 : 10분)</p>
+            </button>
+          </div>
+          {/* 주소 */}
+          <div className="pay_adress_container">
+            <div className="pay_adress_left">--</div>
+            <div className="pay_adress_center">
+              <p>익산점 픽업</p>
+              <p className="pay_tabsubtitle font_02">
+                전라북도 익산시 익산대로 460
+              </p>
+            </div>
+            <div className="pay_adress_right2">--</div>
+          </div>
 
-        <div className="pay_choice_container">
-          <Tabs isFitted variant="enclosed" bg={"white"}>
-            <TabList mb="1em">
-              <Tab fontSize={"1.4rem"}>
-                <Link to={"/payment_delivery"}>
-                  배달 <p className="pay_tabsubtitle">(예상시간 : 40 ~ 50분)</p>
-                </Link>
-              </Tab>
-              <Tab fontSize={"1.4rem"}>
-                포장 <p className="pay_tabsubtitle"> (픽업시간 : 10분)</p>
-              </Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel></TabPanel>
-              {/* 포장 */}
-              <TabPanel>
-                {/* 주소 */}
-                <div className="pay_adress_container">
-                  <div className="pay_adress_left">--</div>
-                  <div className="pay_adress_center">
-                    <p>익산점 픽업</p>
-                    <p className="pay_tabsubtitle font_02">
-                      전라북도 익산시 익산대로 460
-                    </p>
-                  </div>
-                  <div className="pay_adress_right2">--</div>
-                </div>
+          {/* 장바구니 */}
+          <div className="pay_menu_container">
+            <div className="pay_menu_box">
+              <MenuCard />
+              <MenuCard />
+              <MenuCard />
+            </div>
+            <div className="pay_menu_add">
+              <Link to={"/"}>메뉴 추가하기</Link>
+            </div>
+          </div>
 
-                {/* 장바구니 */}
-                <div className="pay_menu_container">
-                  <div className="pay_menu_box">
-                    <MenuCard />
-                    <MenuCard />
-                    <MenuCard />
-                  </div>
-                  <div className="pay_menu_add">
-                    <Link to={"/"}>메뉴 추가하기</Link>
-                  </div>
-                </div>
+          {/* 요청사항 */}
+          <div className="pay_request_container_pick">
+            <p className="pay_req_title">요청사항</p>
+            <div className="pay_req_default01 font_02">
+              <Checkbox
+                border={"gray"}
+                colorScheme="blue"
+                size="lg"
+                spacing="1rem"
+              >
+                일회용 수저, 포크가 필요해요.
+              </Checkbox>
+            </div>
+            <div className="pay_req_choice">
+              <div className="request_directbox">
+                <p>직접 입력</p>
+                <textarea
+                  className="request_directinput"
+                  value={customInput}
+                  onChange={handleCustomInputChange}
+                  placeholder="요청사항을 입력해주세요."
+                />
+                <p>
+                  {customInput.length}/{maxLength}
+                </p>
+              </div>
+            </div>
+          </div>
 
-                {/* 요청사항 */}
-                <div className="pay_request_container_pick">
-                  <p className="pay_req_title">요청사항</p>
-                  <div className="pay_req_default01 font_02">
-                    <Checkbox
-                      border={"gray"}
-                      colorScheme="blue"
-                      size="lg"
-                      spacing="1rem"
-                    >
-                      일회용 수저, 포크가 필요해요.
-                    </Checkbox>
-                  </div>
-                  <div className="pay_req_choice">
-                    {/* <Select
-                      placeholder="요청사항을 선택해주세요."
-                      bg={"initial"}
-                      fontSize={"1.2rem"}
-                      color={"gray"}
-                      size={"lg"}
-                      mt={"15px"}
-                      border={"none"}
-                    >
-                      <option value="option1">직접 입력</option>
-                    </Select> */}
-                    <div className="request_directbox">
-                      <p>직접 입력</p>
-                      <textarea
-                        className="request_directinput"
-                        value={customInput}
-                        onChange={handleCustomInputChange}
-                        placeholder="요청사항을 입력해주세요."
-                      />
-                      <p>
-                        {customInput.length}/{maxLength}
-                      </p>
-                    </div>
-                  </div>
-                </div>
+          {/* 결제 수단 */}
+          <div className="pay_paymethod_container">
+            <p className="pay_method_title">결제수단</p>
+            <div className="pay_methid_radio">
+              <RadioGroup onChange={setPaymentValue} value={paymentValue}>
+                <Stack direction="row">
+                  <Radio
+                    value="1"
+                    size={"lg"}
+                    m={"auto"}
+                    spacing={"3"}
+                    border={"1px solid #d8d9da"}
+                  >
+                    <p className="pay_radio01 font_02">신용 카드</p>
+                  </Radio>
+                </Stack>
+              </RadioGroup>
+            </div>
+          </div>
 
-                {/* 결제 수단 */}
-                <div className="pay_paymethod_container">
-                  <p className="pay_method_title">결제수단</p>
-                  <div className="pay_methid_radio">
-                    <RadioGroup onChange={setPaymentValue} value={paymentValue}>
-                      <Stack direction="row">
-                        <Radio
-                          value="1"
-                          size={"lg"}
-                          m={"auto"}
-                          spacing={"3"}
-                          border={"1px solid #d8d9da"}
-                        >
-                          <p className="pay_radio01 font_02">신용 카드</p>
-                        </Radio>
-                      </Stack>
-                    </RadioGroup>
-                  </div>
-                </div>
+          {/* 금액 */}
+          <div className="pay_amount_container">
+            <div className="pay_amount_detailbox">
+              <p className="pay_detailkind">상품금액</p>
+              <p className="pay_detailcash">{goodPrice} 원</p>
+            </div>
+            <div className="pay_amount_totalbox">
+              <p className="pay_total">총 결제금액</p>
+              <p className="pay_totalcash">{goodPrice} 원</p>
+            </div>
+          </div>
 
-                {/* 금액 */}
-                <div className="pay_amount_container">
-                  <div className="pay_amount_detailbox">
-                    <p className="pay_detailkind">상품금액</p>
-                    <p className="pay_detailcash">{goodPrice} 원</p>
-                  </div>
-                  <div className="pay_amount_totalbox">
-                    <p className="pay_total">총 결제금액</p>
-                    <p className="pay_totalcash">{goodPrice} 원</p>
-                  </div>
-                </div>
-
-                {/* 버튼 */}
-                <Box
-                  as="button"
-                  width={"300px"}
-                  fontSize={"1.2rem"}
-                  p={3}
-                  color="white"
-                  fontWeight="reguiar"
-                  borderRadius="lg"
-                  bgGradient="linear(to-t, blue.500, blue.400, blue.500)"
-                  _hover={{
-                    bgGradient: "linear(to-t,  blue.400, blue.300, blue.400)",
-                  }}
-                >
-                  결제하기
-                </Box>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+          {/* 버튼 */}
+          <button className="_payment_paybtn">결제하기</button>
         </div>
       </div>
 
